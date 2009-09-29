eb162846e1783ac21a206761e2b3e0d0a79ba94f
Lincoln Stoll <lstoll@lstoll.net>
2009-09-29 21:16:10 +0200

    Moving to 0017 Libraries.
    --HG--
    extra : convert_revision : 7e5c6e6ada3c9167a51faae282ba186a81990c63

--- Ethernet/Client.cpp
+++ Ethernet/Client.cpp
@@ -2,7 +2,10 @@ extern "C" {
   #include "types.h"
   #include "w5100.h"
   #include "socket.h"
+  #include "string.h"
 }
+
+#include "WProgram.h"
 
 #include "Ethernet.h"
 #include "Client.h"
@@ -17,14 +20,18 @@ Client::Client(uint8_t sock) {
 Client::Client(uint8_t *ip, uint16_t port) {
   _ip = ip;
   _port = port;  
+  _sock = 255;
 }
 
 uint8_t Client::connect() {
-  _sock = 255;
+  if (_sock != 255)
+    return 0;
   
   for (int i = 0; i < MAX_SOCK_NUM; i++) {
-    if (getSn_SR(i) == SOCK_CLOSED) {
+    uint8_t s = getSn_SR(i);
+    if (s == SOCK_CLOSED || s == SOCK_FIN_WAIT) {
       _sock = i;
+      break;
     }
   }
   
@@ -32,26 +39,44 @@ uint8_t Client::connect() {
     return 0;
     
   _srcport++;
-    
-  socket(_sock, Sn_MR_TCP, 1024 + _srcport, 0);
+  if (_srcport + 1024 == 0) _srcport = 0;
+  socket(_sock, Sn_MR_TCP, _srcport + 1024, 0);
   
-  if (!::connect(_sock, _ip, _port))
+  if (!::connect(_sock, _ip, _port)) {
+    _sock = 255;
     return 0;
+  }
     
   while (status() != SOCK_ESTABLISHED) {
-    if (status() == SOCK_CLOSED)
+    delay(1);
+    if (status() == SOCK_CLOSED) {
+      _sock = 255;
       return 0;
+    }
   }
   
   return 1;
 }
 
 void Client::write(uint8_t b) {
-  send(_sock, &b, 1);
+  if (_sock != 255)
+    send(_sock, &b, 1);
+}
+
+void Client::write(const char *str) {
+  if (_sock != 255)
+    send(_sock, (const uint8_t *)str, strlen(str));
+}
+
+void Client::write(const uint8_t *buf, size_t size) {
+  if (_sock != 255)
+    send(_sock, buf, size);
 }
 
 int Client::available() {
-  return getSn_RX_RSR(_sock);
+  if (_sock != 255)
+    return getSn_RX_RSR(_sock);
+  return 0;
 }
 
 int Client::read() {
@@ -68,18 +93,41 @@ void Client::flush() {
 }
 
 void Client::stop() {
-  close(_sock);
+  if (_sock == 255)
+    return;
+  
+  // attempt to close the connection gracefully (send a FIN to other side)
   disconnect(_sock);
+  unsigned long start = millis();
+  
+  // wait a second for the connection to close
+  while (status() != SOCK_CLOSED && millis() - start < 1000)
+    delay(1);
+    
+  // if it hasn't closed, close it forcefully
+  if (status() != SOCK_CLOSED)
+    close(_sock);
+  
   EthernetClass::_server_port[_sock] = 0;
+  _sock = 255;
 }
 
 uint8_t Client::connected() {
-  uint8_t s = status();
-  return !(s == SOCK_LISTEN || s == SOCK_CLOSED || (s == SOCK_CLOSE_WAIT && !available()));
+  if (_sock == 255) {
+    return 0;
+  } else {
+    uint8_t s = status();
+    return !(s == SOCK_LISTEN || s == SOCK_CLOSED || s == SOCK_FIN_WAIT ||
+      (s == SOCK_CLOSE_WAIT && !available()));
+  }
 }
 
 uint8_t Client::status() {
-  return getSn_SR(_sock);
+  if (_sock == 255) {
+    return SOCK_CLOSED;
+  } else {
+    return getSn_SR(_sock);
+  }
 }
 
 // the next three functions are a hack so we can compare the client returned
